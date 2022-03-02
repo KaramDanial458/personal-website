1f34353683bacc78a32655c82ebfcb2d108609f4
KaramDanial458 <KaramDanial@hotmail.com>
2022-03-01 22:43:46 -0500

    Updates

---
+++ precache-manifest.4596fe5a4ec36c4973d2b77bb83753c4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7194df140f7fdb8978ba",
+    "url": "/personal-website/static/css/main.7a5c1aaa.chunk.css"
+  },
+  {
+    "revision": "7194df140f7fdb8978ba",
+    "url": "/personal-website/static/js/main.7194df14.chunk.js"
+  },
+  {
+    "revision": "1b77d2017dc5ff1fd7f1",
+    "url": "/personal-website/static/js/1.1b77d201.chunk.js"
+  },
+  {
+    "revision": "d4a971169ed339d42abf",
+    "url": "/personal-website/static/js/2.d4a97116.chunk.js"
+  },
+  {
+    "revision": "42ec6260845f3b6b036c",
+    "url": "/personal-website/static/css/3.81321976.chunk.css"
+  },
+  {
+    "revision": "42ec6260845f3b6b036c",
+    "url": "/personal-website/static/js/3.42ec6260.chunk.js"
+  },
+  {
+    "revision": "168d8efb9ecdc4e68917",
+    "url": "/personal-website/static/js/4.168d8efb.chunk.js"
+  },
+  {
+    "revision": "8643af086d60bcefa09f",
+    "url": "/personal-website/static/js/5.8643af08.chunk.js"
+  },
+  {
+    "revision": "c481212d7607808ff98a",
+    "url": "/personal-website/static/js/runtime~main.c481212d.js"
+  },
+  {
+    "revision": "7499834b4d553bf43d561b681e2e80c4",
+    "url": "/personal-website/static/media/ResumeV1.7499834b.png"
+  },
+  {
+    "revision": "aa6f7e9d1011ac086a778e65b3368edd",
+    "url": "/personal-website/static/media/MyWebsiteProfile.aa6f7e9d.jpg"
+  },
+  {
+    "revision": "6331351ac9801f9675dbb397ce7c215b",
+    "url": "/personal-website/static/media/WebsiteHKBG1.6331351a.png"
+  },
+  {
+    "revision": "f0242c25d6bce62ea50a1d03917bf458",
+    "url": "/personal-website/static/media/CityOfTears.f0242c25.mp4"
+  },
+  {
+    "revision": "1339c6a677eb1bfe0fc1ec62fbce5e1c",
+    "url": "/personal-website/static/media/ShineSprite2.1339c6a6.png"
+  },
+  {
+    "revision": "37660271bebba1a08d71055dc9ab08f5",
+    "url": "/personal-website/static/media/ResumeBackground.37660271.png"
+  },
+  {
+    "revision": "baec67739cb1cc20cd0e22f5776d2121",
+    "url": "/personal-website/static/media/ResumeImageInverted.baec6773.png"
+  },
+  {
+    "revision": "47922c47a3304496a8c97012950d48c5",
+    "url": "/personal-website/index.html"
+  }
+];
